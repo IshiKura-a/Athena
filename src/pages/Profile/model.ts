c4fa1d5c50d9795b63d8209dd1c275e84645882b
Holly <60909316+Hollie7@users.noreply.github.com>
2021-04-19 01:54:10 +0800

    修改个人信息页面

--- src/pages/Profile/model.ts
+++ src/pages/Profile/model.ts
@@ -2,9 +2,11 @@ import { action, observable } from 'mobx';
 import { BaseStore } from '@/store';
 
 export default class ProfileStore {
-  @observable name = 'ssa';
+  @observable name = '张天然';
   @observable id = '3180102086';
   @observable department = '计算机科学与技术';
+  @observable telephone = '15336527322';
+  @observable email = 'holly1027@zju.edu.cn';
 
   baseStore: BaseStore;
   constructor(baseStore: BaseStore) {
@@ -21,5 +23,23 @@ export default class ProfileStore {
 
   @action setDepartment(department: string) {
     this.department = department;
+  }
+
+  @action setTelephone(telephone: string) {
+    this.telephone = telephone;
+  }
+
+  @action setEmail(email: string) {
+    this.email = email;
+  }
+
+  @action editEmail = async(email) => {
+     // console.log("edit telephone");
+     this.email = email;
+  }
+
+  @action editTelephone = async(telephone) => {
+    // console.log("edit telephone");
+    this.telephone = telephone;
   }
 }
