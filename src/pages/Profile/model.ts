b2887634db9878ee167317fb510ca67b04a8c0e6
hollie7 <1139872552@qq.com>
2021-05-11 22:42:31 +0800

    完善了上次的需求
    增加了status管理，修改gender的svg图标

--- src/pages/Profile/model.ts
+++ src/pages/Profile/model.ts
@@ -2,9 +2,18 @@ import { action, observable } from 'mobx';
 import { BaseStore } from '@/store';
 
 export default class ProfileStore {
-  @observable name = 'ssa';
+  @observable name = '张天然';
   @observable id = '3180102086';
   @observable department = '计算机科学与技术';
+  @observable telephone = '15336527322';
+  @observable email = 'holly1027@zju.edu.cn';
+  @observable wechat = '15336527322';
+  @observable qq = '1139872552';
+  @observable gender = '女';
+  @observable campus = '玉泉';
+  @observable birthday = '2000-10-27';
+  @observable blood = 'O';
+  @observable status = 2;
 
   baseStore: BaseStore;
   constructor(baseStore: BaseStore) {
@@ -22,4 +31,25 @@ export default class ProfileStore {
   @action setDepartment(department: string) {
     this.department = department;
   }
+
+  @action editEmail = async (email: string) => {
+    this.email = email;
+  };
+
+  @action editTelephone = async (telephone: string) => {
+    this.telephone = telephone;
+  };
+
+  @action editWechat = async (wechat: string) => {
+    this.wechat = wechat;
+  };
+
+  @action editQQ = async (qq: string) => {
+    this.qq = qq;
+  };
+
+  @action editStatus = async (status: number) => {
+    console.log('back', status);
+    this.status = status;
+  };
 }
