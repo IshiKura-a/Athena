517738eddf9c58f631c866d64f000dcef403e9de
IshiKura-a <1563415484@qq.com>
2021-07-10 03:25:36 +0800

    Revert "Revert "feat(profile_new):修复bug+修改数据联调""
    This reverts commit 564feec020d6f4492da9be4ed803034b627218b0.

--- src/services/profile.ts
+++ src/services/profile.ts
@@ -2,9 +2,13 @@ import request from '@/utils/request';
 
 export type ProfileParamsType = {
   id: string;
+  phone?: string;
+  wechat?: string;
+  email?: string;
+  qq?: string;
 };
 
-export async function getProfileInfo(params: ProfileParamsType) {
+export async function getProfileInfo(params: { id: string }) {
   return request(`/api/student/`, {
     method: 'GET',
     params,
@@ -13,6 +17,13 @@ export async function getProfileInfo(params: ProfileParamsType) {
 
 export async function getProfileInfoNoToken() {
   return request(`/api/student0`);
+}
+
+export async function updateProfileInfo(params: ProfileParamsType) {
+  return request(`/api/student/update`, {
+    method: 'POST',
+    data: params,
+  });
 }
 
 export default { getProfileInfo };
